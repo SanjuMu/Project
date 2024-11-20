51baf1e524af07c3d6fa8c83d13ce73f624d2d42
SanjuMu <sanjusri.munigala@gmail.com>
2024-11-19 20:56:07 -0500

    summer_times

---
+++ summer_times.py
@@ -0,0 +1,7 @@
+temp = int(input("Enter the temperature outside here:"))
+
+if (temp>=16):
+    print("There is no need to wear a jacket, it's warm outside")
+
+else :
+    print("Wear a jacket, it's cold outside")
